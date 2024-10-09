bdde29bd973badb0d2ce59827974d9e6fc167f56
Tim <tulxoro@hotmail.com>
2024-10-08 17:08:35 -0700

    updated mobile view and added top section of window

--- app/src/lib/component/tim-os/shortcut.svelte
+++ app/src/lib/component/tim-os/shortcut.svelte
@@ -1,12 +1,22 @@
 <script>
+	import { createEventDispatcher } from "svelte";
+
 	export let name = 'null';
 	export let icon = 'https://via.placeholder.com/50';
+	export let window = 'null';
 
 	export let selected = false;
 	let hovered = false;
+
+	const dispatch = createEventDispatcher();
+
+	function handleClick() {
+		dispatch('click', { name });
+	}
+
 </script>
 
-<button class="grid hover:bg-slate-600 hover:bg-opacity-75 w-24 h-20">
+<button class="grid hover:bg-slate-600 hover:bg-opacity-75 w-24 h-20" on:click={handleClick}>
 	<img class="m-auto" src="{icon}" alt="Placeholder" />
 	<span class="text-center">{name}</span>
 </button>
---
+++ app/src/lib/component/tim-os/window.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+	export let name = 'null';
+
+    const dispatch = createEventDispatcher();
+
+    function closeWindow() {
+        dispatch('close', { name });
+    }
+
+</script>
+
+<div class="bg-gray-800 absolute z-100">
+	<!-- Top bar -->
+	<div class="flex justify-between items-center p-4">
+		<div class="flex items-center space-x-4 w-96">
+			<button class="bg-red-500 h-3 w-4 rounded-full" on:click={closeWindow} />
+			<button class="bg-yellow-500 h-3 w-4 rounded-full" on:click={closeWindow}/>
+			<button class="bg-green-500 h-3 w-4 rounded-full" on:click={closeWindow}/>
+            <span class="text-neutral-50 text-center select-none w-full">{name}</span>
+		</div>
+		
+	</div>
+</div>
